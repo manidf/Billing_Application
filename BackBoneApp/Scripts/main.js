f9a4d58bee907ab411da994915d1abde04a10c5e
Mannuel <mannuel.ferreira@gmail.com>
2014-02-04 05:02:20 +0200

    View, Model, Templates
    small edits, built views, the model and template using underscore.js

--- BackBoneApp/Scripts/main.js
+++ BackBoneApp/Scripts/main.js
@@ -19,13 +19,51 @@
             }
         });
 
+        //Define the view that will render teh model
+        var PreviewInvoiceItemView = Backbone.View.extend({
+
+            //define template using template engine from underscore.js
+            template: _.template('\
+              Price: <%= price %>.\
+              Quantity: <%= quantity %>.\
+              Amount: <%= amount %>.\
+            '),
+
+            //render view
+            render: function () {
+
+                //generate HTML by rendering the template.
+                var html = this.template({
+
+                    //Pass model properties to the template.
+                    price: this.model.get('price'),
+                    quantity: this.model.get('quantity'),
+
+                    //calculate amount and pass it to the template.
+                    amount: this.model.calculateAmount()
+                });
+
+                //set html for the view element using jQuery.
+                $(this.el).html(html);
+            }
+        });
+
+    });
+    $(document).ready(function () {
         //Create Model Instance
         var invoiceItemModel = new InvoiceItemModel({
             price: 2,
             quantity: 3
         });
 
+        var previewInvoiceItemView = new PreviewInvoiceItemView({
+            model: invoiceItemModel,
+            el: 'body'
+        });
+
+        previewInvoiceItemView.render();
 
     });
+    
 
 })(jQuery);
