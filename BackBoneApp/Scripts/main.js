13947aabb5e828327cf207788db6cfd165c7e45c
Mannuel <mannuel.ferreira@gmail.com>
2014-02-04 04:17:08 +0200

    first commit
    Billing application, built using backbone js

---
+++ BackBoneApp/Scripts/main.js
@@ -0,0 +1,31 @@
+﻿(function ($) {
+
+    //objec declarations go here
+    $(document).ready(function () {
+
+
+        //start application code here
+
+        //Define Model
+        var InvoiceItemModel = Backbone.Model.extend({
+            //set defaults
+            defaults: {
+                price: 0,
+                quantity: 0
+            },          
+            //calculate amount
+            calculateAmount: function () {
+                return this.get('price') * this.get('quantity');
+            }
+        });
+
+        //Create Model Instance
+        var invoiceItemModel = new InvoiceItemModel({
+            price: 2,
+            quantity: 3
+        });
+
+
+    });
+
+})(jQuery);
